5758443ff08f1ef3f14cfe1189a1f015f84e9cf3
zagorky <zagorkiynahyi@gmail.com>
2024-09-22 15:31:16 +0300

    feat: solve the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,20 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  guess() {
+    return (this.solution = Math.round((this.min + this.max) / 2));
+  }
+  lower() {
+    return (this.max = this.solution);
+  }
+  greater() {
+    return (this.min = this.solution);
+  }
 }
 
 module.exports = GuessingGame;
